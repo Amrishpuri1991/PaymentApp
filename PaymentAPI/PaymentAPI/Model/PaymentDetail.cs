b4494fe733f1c28fa6947e8230ebeddf0afdf7e1
Amrishpuri1991 <puridigitallab@gmail.com>
2021-02-20 15:25:33 +1100

    Backend with .net MVC framework API

---
+++ PaymentAPI/PaymentAPI/Model/PaymentDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Model
+{
+    public class PaymentDetail
+    {
+    [Key]        
+    public int PaymentDetailID { get; set; }
+    
+    [Column(TypeName="nvarchar(100)")]        
+    public String CardOwnerName { get; set; }
+        
+    [Column(TypeName = "nvarchar(16)")]
+    public String CardNumber { get; set; }
+
+    [Column(TypeName = "nvarchar(5)")]
+    public string ExpirationDate { get; set; }
+
+    [Column(TypeName = "nvarchar(3)")]
+    public string SecurityCode { get; set; }
+
+
+    }
+}
